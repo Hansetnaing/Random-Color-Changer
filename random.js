b2b352a33dcdecd4388138f03db0bd57ee63a10a
unknown <minhansetnaing11@gmail.com>
2025-05-24 21:54:14 +0630

    Done

--- random.js
+++ random.js
@@ -3,38 +3,38 @@ let history = [];
 
 // Start Change Color
 
-function changeColor(){
-    let color;
+function changeColor() {
+  let color;
 
-    if(isRGB){
-        let r = Math.floor(Math.random() * 256); //random value
-        let g = Math.floor(Math.random() * 256);
-        let b = Math.floor(Math.random() * 256);
+  if (isRGB) {
+    let r = Math.floor(Math.random() * 256); //random value
+    let g = Math.floor(Math.random() * 256);
+    let b = Math.floor(Math.random() * 256);
 
-        color = `rgb(${r}, ${g}, ${b})`;
-    }
+    color = `rgb(${r}, ${g}, ${b})`;
+  }
 
-    else{
-        color = "#" + Math.floor(Math.random() * 16777215).toString(16).padStart(6,'0');
-    }
+  else {
+    color = "#" + Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0');
+  }
 
-    document.body.style.backgroundColor = color;
-    // document.getElementById('colorCode').style.color = color;
-    document.getElementById('colorCode').textContent = color;
-    document.body.style.transition = "all ease-in-out .5s";
+  document.body.style.backgroundColor = color;
+  // document.getElementById('colorCode').style.color = color;
+  document.getElementById('colorCode').textContent = color;
+  document.body.style.transition = "all ease-in-out .5s";
 
-    addToHistory(color);
-    updateColorCode(color);
+  addToHistory(color);
+  updateColorCode(color);
 }
 
 // End Change Color
 
 // Toggle Color Mode 
 
-function toggleMode(){
-    isRGB = !isRGB; // true
-    let button = document.querySelectorAll('button')[1];
-    button.textContent = isRGB ? "Switch to HEX" : "Switch to RGB";
+function toggleMode() {
+  isRGB = !isRGB; // true
+  let button = document.querySelectorAll('button')[1];
+  button.textContent = isRGB ? "Switch to HEX" : "Switch to RGB";
 }
 
 // End Toggle Color Mode 
@@ -43,7 +43,7 @@ function toggleMode(){
 
 
 function addToHistory(color) {
-  
+
 
   if (history.length >= 5) {
     history.pop(color);
@@ -51,12 +51,12 @@ function addToHistory(color) {
 
   history.unshift(color);
 
-  let historyDiv = document.getElementById("history"); 
+  let historyDiv = document.getElementById("history");
   historyDiv.innerHTML = '<strong>History:</strong>';
   history.forEach(x => {
 
     let span = document.createElement("span");
-    
+
     span.textContent = x;
     span.style.backgroundColor = x;
     span.classList.add("updateColor");
@@ -70,7 +70,7 @@ function addToHistory(color) {
 
 // Start Apply Color 
 
-function applyColor(color){
+function applyColor(color) {
   document.body.style.backgroundColor = color;
   document.getElementById('colorCode').textContent = color;
 }
@@ -79,9 +79,9 @@ function applyColor(color){
 
 // Start Copy Color
 
-function copyColor(){
-    let color = document.getElementById('colorCode').textContent;
-    navigator.clipboard.writeText(color).then(() => alert('Copied :'+ color));
+function copyColor() {
+  let color = document.getElementById('colorCode').textContent;
+  navigator.clipboard.writeText(color).then(() => alert('Copied :' + color));
 }
 
 // End Copy Color
